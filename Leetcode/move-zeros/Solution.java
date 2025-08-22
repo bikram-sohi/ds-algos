c5d62e00c88cae8d9703cb847f775a8d5fd3a02f
Bikram Sohi <bkrmsohi@gmail.com>
2025-08-21 19:09:56 -0600

    Leetcode move zeros

---
+++ Leetcode/move-zeros/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (count < i) {
+                    nums[count] = nums[i];
+                    nums[i] = 0;
+                }
+                count++;
+            }
+        }
+    }
+}
