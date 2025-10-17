f154fd2cae1b1d9528c32a2504a3757bc3f561a0
Bikram Sohi <bkrmsohi@gmail.com>
2025-10-17 12:46:34 -0600

    String compression

---
+++ Leetcode/string-compression/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int compress(char[] chars) {
+        int ans = 0;
+        for(int i = 0; i < chars.length;) {
+            char cur = chars[i];
+            int count = 0;
+
+            while(i < chars.length && cur == chars[i]) {
+                i++;
+                count++;
+            }
+
+            chars[ans++] = cur;
+
+            if(count > 1) {
+                for(char c : String.valueOf(count).toCharArray()) {
+                    chars[ans++] = c;
+                }
+            }
+        }
+        return ans;
+    }
+}
