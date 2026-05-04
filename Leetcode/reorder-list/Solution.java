1a1926b39eb1021a71476fa562bcc77bc08ffa99
Bikram Sohi <bkrmsohi@gmail.com>
2026-05-03 20:30:29 -0600

    Reorder list

---
+++ Leetcode/reorder-list/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode curr = slow.next;
+        slow.next = null;
+        ListNode next = null;
+        ListNode prev = null;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode firstHead = head;
+        ListNode secondHead = prev;
+
+        while(secondHead != null) {
+            ListNode temp1 = firstHead.next;
+            ListNode temp2 = secondHead.next;
+
+            firstHead.next = secondHead;
+            secondHead.next = temp1;
+
+            firstHead = temp1;
+            secondHead = temp2;
+        }
+    }
+}
